fc36e6b5a1ea250b71773fd050fc1f73b0b7b666
izabela1337 <54004218+izabela1337@users.noreply.github.com>
2020-06-05 15:11:03 +0200

    initial setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import kek from "./kek";
+
+function app() {
+  var peniser = 0;
+
+  return (
+    <div>
+      <button>{peniser}</button>
+      <kek />
+    </div>
+  );
+}
+
+export default app;
---
+++ src/kek.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function kek() {
+  const saypenis = () => {
+    console.log("penis");
+  };
+  return (
+    <div>
+      <h1>I want to die stuffed with penises</h1>
+      <button onClick={saypenis}>say penis!</button>
+    </div>
+  );
+}
+export default kek;
